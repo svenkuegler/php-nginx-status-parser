c0ba035bbb3c4b78693e516e7f4ddacfa2a9caeb
Sven Kügler <sven.kuegler@gmail.com>
2016-07-26 16:35:24 +0200

    add parser functionality

--- src/NginxStatusParser.php
+++ src/NginxStatusParser.php
@@ -44,11 +44,43 @@ class NginxStatusParser {
 
     /**
      * @return NginxStatus
+     * @throws NginxStatusException
      */
     public function parse() {
 
         $result = new NginxStatus();
 
+        if(!count($this->statusPageContent) == 4) {
+            throw new NginxStatusException("Result seems to be invalid!", 3);
+        }
+
+        $tmpRow = array();
+        for($i=0; $i<count($this->statusPageContent); $i++) {
+            preg_match($this->_getRegex()[$i], $this->statusPageContent[$i], $tmpRow[$i]);
+        }
+
+        $result->setActiveConnections($tmpRow[0][1]);
+
+        $result->setAcceptedRequests($tmpRow[2][1]);
+        $result->setHandledRequests($tmpRow[2][3]);
+        $result->setRequestsPerConnection($tmpRow[2][1] / $tmpRow[2][3]);
+
+        $result->setStatusReading($tmpRow[3][1]);
+        $result->setStatusWriting($tmpRow[3][2]);
+        $result->setStatusWaiting($tmpRow[3][3]);
+
         return $result;
+    }
+
+    /**
+     * @return array
+     */
+    private function _getRegex() {
+        return array(
+            "/Active connections\: ([0-9]+)/i",
+            "/server accepts handled requests/i",
+            "/ ([0-9]+) ([0-9]+) ([0-9]+)/i",
+            "/Reading: ([0-9]+) Writing: ([0-9]+) Waiting: ([0-9]+)/i",
+        );
     }
 }
